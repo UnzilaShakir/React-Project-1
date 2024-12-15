2c0a89bf5746dac31552dd35a389178be55be94d
unzilashakir <unzilashakir21@gmail.com>
2024-12-15 16:08:22 +0500

    First Project

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import react from "react";
+import ReactDom from "react-dom/client";
+
+const rootDiv = document.getElementById("root");
+
+ReactDom.createRoot(rootDiv).render(
+  <div>
+    <h1>Welcome to React</h1>
+    <h2>This is a React boiler plate</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, architecto! Similique sint blanditiis eaque officiis cumque, nam
+      consectetur excepturi quas doloribus vitae ex repudiandae, quam fugiat odio mollitia soluta deleniti. Consectetur minus beatae a ab nulla
+      rem optio pariatur. Repellendus facilis corporis culpa tenetur incidunt? Error sit eius deleniti ut quia. Fugit expedita cum nulla corporis
+      molestiae impedit voluptatem possimus cumque laudantium voluptas illum nisi quas, vel fuga earum soluta alias ducimus sequi, accusamus
+      beatae minus nemo sunt perspiciatis. Corrupti numquam vitae, eum nesciunt illum vel eius quasi expedita quo nisi placeat deserunt provident
+      optio. A iste asperiores sequi ratione excepturi eos et hic repellendus, eligendi optio debitis vel repellat totam dignissimos quo
+      molestias sunt aut voluptatum incidunt necessitatibus amet consequuntur quos. Porro qui et eveniet quia doloribus consequuntur dolores
+      laudantium molestiae provident ab quo facere minima voluptate commodi nobis accusantium sapiente consequatur nihil cupiditate, sunt hic
+      adipisci suscipit officia explicabo! Quia libero accusantium consectetur dolorum consequatur maiores laboriosam ea. Dolore quaerat earum
+      dicta quidem voluptas similique aperiam officia maxime delectus cumque excepturi nostrum architecto distinctio nobis placeat blanditiis
+      doloribus ex ipsum sequi autem necessitatibus, consequatur nihil laudantium optio. Deleniti repellat recusandae odio consequatur pariatur
+      . Itaque doloribus consequuntur animi reiciendis fuga cumque molestias cupiditate consequatur optio a! Quas, quasi eos sed excepturi
+      enim rem cupiditate iusto. Ex autem sequi et illo officia nam vitae, quos praesentium asperiores quasi. Enim suscipit hic ad aliquam
+      reprehenderit eum, eveniet itaque dignissimos, cumque veritatis, illum totam distinctio odio dolorum id blanditiis aperiam. Assumenda
+      voluptatibus dolor tenetur ipsam deleniti quod iure laudantium quo esse dolores. Minus laudantium hic fuga autem accusantium eius sed
+      consectetur doloribus itaque laboriosam atque distinctio numquam eaque neque illo sunt cumque, incidunt quae vero eum, repellat provident
+      quaerat? Amet modi perspiciatis blanditiis harum, recusandae odio sequi! Iste sed reprehenderit aut excepturi!</p>
+
+      <img src="/public/vite.svg" alt="" /> 
+
+  </div>
+)
